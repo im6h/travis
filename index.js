b787643604298992287ce38488464f03f8b1e7fc
Vu <tzero1898@gmail.com>
2020-01-28 00:38:04 +0700

    Init commit

--- index.js
+++ index.js
@@ -5,6 +5,11 @@ const app = express();
 app.get('/', (req, res) => {
   res.send('Hello Travis');
 });
+app.post('/', (req, res) => {
+  const body = req.body;
+  console.log(body);
+  res.send(body);
+});
 
 const server = app.listen(3000, () => {
   console.log('App running on port 3000');
