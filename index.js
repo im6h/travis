3d369a27ac7d4099a7930ad6466a8069cb4746b7
Vu <tzero1898@gmail.com>
2020-02-09 19:47:45 +0700

    fix:edit index file

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ app.use('/api/v1', require('./router/router'));
 
 const server = app.listen(3000, () => {
   console.log('App running on port 3000');
+  process.exit();
 });
 
 module.exports = server;
